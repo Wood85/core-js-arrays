364eb7174007f93d8d9963e805645346fa4d29de
Aleksandr <megalistnn@gmail.com>
2024-01-02 18:24:43 +0300

    feat: implement function generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -361,8 +361,15 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = new Array(len);
+  let count = -1;
+  arr.fill(null);
+  const res = arr.map(() => {
+    count += 2;
+    return count;
+  });
+  return res;
 }
 
 /**
