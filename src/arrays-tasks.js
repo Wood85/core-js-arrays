559426ffd4fe7fadf58b2e1b2f8620882bd74093
Aleksandr <megalistnn@gmail.com>
2024-01-02 19:50:58 +0300

    feat: implement function getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -445,8 +445,12 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const result = numbers.map((number, index) => {
+    if (number % 2 !== 0) return index;
+    return undefined;
+  });
+  return result.filter((i) => i !== undefined);
 }
 
 /**
