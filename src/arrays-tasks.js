25fe5fdc3dba3d0742c19b0cee46464d55542b6d
Aleksandr <megalistnn@gmail.com>
2024-01-02 18:33:45 +0300

    feat: implement function sortDigitNamesByNumericOrder

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -561,8 +561,25 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const name = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  const digitArr = arr.map((i) => name[i]);
+  const sortDigitArr = digitArr.sort((a, b) => a - b);
+  const res = sortDigitArr.map((i) =>
+    Object.keys(name).find((key) => name[key] === i)
+  );
+  return res;
 }
 
 /**
