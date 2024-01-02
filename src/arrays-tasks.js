bcf055ba9956f81a692cdb77704550e2e082a372
Aleksandr <megalistnn@gmail.com>
2024-01-02 18:26:59 +0300

    feat: implement function getElementByIndices

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -384,8 +384,13 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  let newArr = arr;
+  indices.map((index) => {
+    newArr = newArr[index];
+    return newArr;
+  });
+  return newArr;
 }
 
 /**
