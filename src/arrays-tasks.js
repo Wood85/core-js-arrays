17e4e92a48407327aa8f92ccb891184e48853349
Aleksandr <megalistnn@gmail.com>
2024-01-03 10:25:23 +0300

    feat: implement function createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -345,8 +345,13 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const countItem = Math.ceil(arr.length / chunkSize);
+  const resArr = new Array(countItem).fill([]);
+  const res = resArr.map((_, index) =>
+    arr.slice(index * chunkSize, (index + 1) * chunkSize)
+  );
+  return res;
 }
 
 /**
